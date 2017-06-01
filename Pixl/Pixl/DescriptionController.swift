71bc8a5c832a4d3886979abf65ba0224aa27a9a4
nathanljustin <njustin@g.hmc.edu>
2017-06-02 01:06:13 +0200

    First draft of storyboard

---
+++ Pixl/Pixl/DescriptionController.swift
@@ -0,0 +1,23 @@
+//
+//  Pixl
+//
+//  Created by Nathan Justin on 6/1/17.
+//  Copyright © 2017 Nathan Justin. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
